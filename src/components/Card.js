96e501d8761b67784ac342706876620c1a6da947
majazocom <maja.gedda@zocom.se>
2021-02-25 13:26:21 +0100

    card flip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,34 @@
+import { Opacity, WbSunnyRounded } from '@material-ui/icons';
 import React, { Component } from 'react';
 import '../style/card.css';
 
 class Card extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            isFlipped: false
+        }
+    }
+
     render() {
         return (
-            <section className="card-container">
-                <img className="card-image" src={this.props.img} height="200px" width="160px" alt={this.props.imgAlt}></img>
-                <h3 className="card-title">{this.props.title}</h3>
-                <p className="card-text">{this.props.text}</p>
-            </section>
+            <div>
+                { !this.state.isFlipped ?
+                    <section onMouseEnter={() => this.setState({ isFlipped: true })} className="card-container front">
+                        <img className="card-image" src={this.props.img} height="200px" width="160px" alt={this.props.imgAlt}></img>
+                        <h3 className="card-title">{this.props.title}</h3>
+                        <p className="card-text">{this.props.text}</p>
+                    </section>
+                    :
+                    <section onMouseLeave={() => this.setState({ isFlipped: false })} className="card-container back">
+                        <Opacity />
+                        <p>Vattning</p>
+                        <WbSunnyRounded />
+                        <p>Soligt läge</p>
+                    </section>
+                }
+            </div>
         )
     }
 }
