2bcb80f3fca6204bb8877ad71660f3c8b37ad60a
majazocom <maja.gedda@zocom.se>
2021-02-25 10:22:28 +0100

    props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import Card from './components/Card';
 
 function App() {
+  const cardImgUrl = "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80";
+  const cardText = "Aloe Vera Plant";
+  const cardImgAlt = "A succulent";
+
   return (
     <section className="App">
       <header className="app-header">Plant app</header>
-      <Card />
+      <main className="cards-container">
+        <Card img={cardImgUrl} imgAlt={cardImgAlt} text={cardText} />
+        <Card img={cardImgUrl} imgAlt={cardImgAlt} text={cardText} />
+        <Card img={cardImgUrl} imgAlt={cardImgAlt} text={cardText} />
+      </main>
     </section>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import '../style/card.css';
 
 class Card extends Component {
     render() {
         return (
-            <section>
-                <img src="https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" height="200px" width="140px" alt="Succulent"></img>
-                <p>Aloe Vera Plant</p>
+            <section className="card-container">
+                <img className="card-image" src={this.props.img} height="200px" width="140px" alt={this.props.imgAlt}></img>
+                <p className="card-text">{this.props.text}</p>
             </section>
         )
     }
