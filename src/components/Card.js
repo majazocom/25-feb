3a06e7139061201e04618fa25e1e417d090286b9
majazocom <maja.gedda@zocom.se>
2021-02-25 09:34:24 +0100

    alt text changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ class Card extends Component {
     render() {
         return (
             <section>
-                <img src="https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" height="200px" width="140px" alt="Image of succulent"></img>
+                <img src="https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" height="200px" width="140px" alt="Succulent"></img>
                 <p>Aloe Vera Plant</p>
             </section>
         )
