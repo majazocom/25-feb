29fb18ccead788231471b46897a2c57099e5d89c
majazocom <maja.gedda@zocom.se>
2021-02-25 10:52:03 +0100

    updated plants.json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import Card from './components/Card';
+import plants from './assets/plants.json';
 
 function App() {
-  const cardImgUrl = "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80";
-  const cardText = "Aloe Vera Plant";
-  const cardImgAlt = "A succulent";
-
   return (
     <section className="App">
       <header className="app-header">Plant app</header>
       <main className="cards-container">
-        <Card img={cardImgUrl} imgAlt={cardImgAlt} text={cardText} />
-        <Card img={cardImgUrl} imgAlt={cardImgAlt} text={cardText} />
-        <Card img={cardImgUrl} imgAlt={cardImgAlt} text={cardText} />
+        {
+          plants.map(plant => <Card key={plant.id} img={plant.url} imgAlt={plant.altUrlText} title={plant.title} text={plant.text} />)
+        }
       </main>
     </section>
   );
